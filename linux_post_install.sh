4d9a7f4801df1b73c39ea1ad9dc9e65b769d813b
Adam Grünwald <adam@mbpsomtlhoradam.lan>
2021-10-21 16:27:43 +0200

    Tried to fix Docker installation

--- linux_post_install.sh
+++ linux_post_install.sh
@@ -33,6 +33,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
 # Create Conda environment
